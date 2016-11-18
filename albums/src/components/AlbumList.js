396105a2bb610acb77acf0fa2377c55739dd5af4
Heloá Meneses <heloa.meneses@gmail.com>
2016-11-18 21:05:12 -0200

    Add http request for albums list

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+	state = { albums: [] };
+
 	componentWillMount() {
-		console.log('componentWillMount in Album');
+		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+			.then(
+				response => this.setState({ albums: response.data }));
 	}
 
 	render() {
