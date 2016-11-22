1d78a85d92fc93d48c3cc36b9d9d491bb07ff4f4
Heloá Meneses <heloa.meneses@gmail.com>
2016-11-22 19:30:31 -0200

    Adding common components, LoginForm and App

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,25 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import {Header} from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyDzkSpCBkVKdjGC2siYvm3r2fJamOQ8vaY',
+    authDomain: 'auth-fe6ed.firebaseapp.com',
+    databaseURL: 'https://auth-fe6ed.firebaseio.com',
+    storageBucket: 'auth-fe6ed.appspot.com',
+    messagingSenderId: '349651074027'
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>An App!</Text>
+        <Header headerText="Authentication"></Header>
+        <LoginForm />
       </View>
     );
   }
